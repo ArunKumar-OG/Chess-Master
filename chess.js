70d07089b36901e595b96dbb7777f4b163f714ca
Arun K. <121557055+ArunKumar-OG@users.noreply.github.com>
2026-02-14 14:46:28 +0530

    Add files via upload

--- chess.js
+++ chess.js
@@ -7,10 +7,22 @@ class ChessGame {
     this.undoStack = [];
     this.redoStack = [];
     this.pendingPromotion = null;
+<<<<<<< HEAD
     this.initialize();
   }
 
   initialize() {
+=======
+    this.enPassantTarget = null;
+    this.castlingRights = {
+      w: { kingside: true, queenside: true },
+      b: { kingside: true, queenside: true }
+    };
+    this.initialize();
+  }
+
+initialize() {
+>>>>>>> 5082604 (Initial commit)
     this.board[0] = ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'];
     this.board[1] = Array(this.SIZE).fill('p');
     this.board[6] = Array(this.SIZE).fill('P');
@@ -21,6 +33,14 @@ class ChessGame {
     this.redoStack = [];
     this.currentPlayer = 'w';
     this.pendingPromotion = null;
+<<<<<<< HEAD
+=======
+    this.enPassantTarget = null;
+    this.castlingRights = {
+      w: { kingside: true, queenside: true },
+      b: { kingside: true, queenside: true }
+    };
+>>>>>>> 5082604 (Initial commit)
   }
 
   getBoardState() {
@@ -55,6 +75,7 @@ class ChessGame {
   }
 
   moveCheck(fromR, fromC, toR, toC, player) {
+<<<<<<< HEAD
     const piece = this.board[fromR][fromC];
     if (piece === ' ' ||
         (player === 'w' && piece !== piece.toUpperCase()) ||
@@ -80,23 +101,155 @@ class ChessGame {
         return this.board[toR][toC] !== ' ' &&
                ((piece === piece.toUpperCase() && this.board[toR][toC] === this.board[toR][toC].toLowerCase()) ||
                 (piece === piece.toLowerCase() && this.board[toR][toC] === this.board[toR][toC].toUpperCase()));
+=======
+  const piece = this.board[fromR][fromC];
+  if (piece === ' ' ||
+    (player === 'w' && piece !== piece.toUpperCase()) ||
+    (player === 'b' && piece !== piece.toLowerCase()) ||
+    this.isSameColorPiece(toR, toC, player)) return false;
+
+  const type = piece.toUpperCase();
+
+  if (type === 'R' && (fromR === toR || fromC === toC)) return this.isPathClear(fromR, fromC, toR, toC);
+
+  if (type === 'N' && ((Math.abs(fromR - toR) === 2 && Math.abs(fromC - toC) === 1) ||
+    (Math.abs(fromR - toR) === 1 && Math.abs(fromC - toC) === 2))) return true;
+
+  if (type === 'B' && Math.abs(fromR - toR) === Math.abs(fromC - toC)) return this.isPathClear(fromR, fromC, toR, toC);
+
+  if (type === 'Q' && ((fromR === toR || fromC === toC) ||
+    Math.abs(fromR - toR) === Math.abs(fromC - toC))) return this.isPathClear(fromR, fromC, toR, toC);
+
+  if (type === 'K') {
+    if (Math.abs(fromR - toR) <= 1 && Math.abs(fromC - toC) <= 1) return true;
+    if (Math.abs(fromC - toC) === 2 && fromR === toR) {
+      return this.canCastle(fromR, fromC, toR, toC, player);
+    }
+  }
+
+  if (type === 'P') {
+    const dir = piece === piece.toUpperCase() ? -1 : 1;
+    const startRow = piece === piece.toUpperCase() ? 6 : 1;
+
+    // Forward move
+    if (fromC === toC && this.board[toR][toC] === ' ') {
+      if (toR === fromR + dir) return true;
+      if (fromR === startRow && toR === fromR + 2 * dir) {
+        return this.board[fromR + dir][fromC] === ' ' && this.board[toR][toC] === ' ';
+      }
+    }
+
+    // En Passant
+    if (
+      Math.abs(fromC - toC) === 1 &&
+      toR === fromR + dir &&
+      this.board[toR][toC] === ' ' &&
+      this.enPassantTarget &&
+      this.enPassantTarget.row === toR &&
+      this.enPassantTarget.col === toC
+    ) {
+      return true;
+    }
+
+    // Diagonal capture
+    if (Math.abs(fromC - toC) === 1 && toR === fromR + dir) {
+      if (this.board[toR][toC] !== ' ') {
+        return (
+          (piece === piece.toUpperCase() && this.board[toR][toC] === this.board[toR][toC].toLowerCase()) ||
+          (piece === piece.toLowerCase() && this.board[toR][toC] === this.board[toR][toC].toUpperCase())
+        );
+      }
+    }
+  }
+
+  return false;
+}
+
+      
+
+  canCastle(fromR, fromC, toR, toC, player) {
+    const isKingside = toC > fromC;
+    if (!this.castlingRights[player][isKingside ? 'kingside' : 'queenside']) {
+      return false;
+    }
+
+    const step = isKingside ? 1 : -1;
+    let c = fromC + step;
+    const targetC = isKingside ? 7 : 0;
+
+    while (c !== targetC) {
+      if (this.board[fromR][c] !== ' ') return false;
+      c += step;
+    }
+
+    if (this.board[fromR][targetC].toUpperCase() !== 'R') return false;
+
+    const opponent = player === 'w' ? 'b' : 'w';
+    c = fromC;
+    const stepsToCheck = isKingside ? 3 : 2;
+
+    for (let i = 0; i < stepsToCheck; i++) {
+      if (this.isSquareUnderAttack(fromR, c, opponent)) return false;
+      c += step;
+    }
+
+    return true;
+  }
+
+  isSquareUnderAttack(row, col, byPlayer) {
+    for (let r = 0; r < this.SIZE; r++) {
+      for (let c = 0; c < this.SIZE; c++) {
+        const piece = this.board[r][c];
+        if (piece !== ' ' &&
+          ((byPlayer === 'w' && piece === piece.toUpperCase()) ||
+            (byPlayer === 'b' && piece === piece.toLowerCase()))) {
+          if (this.moveCheck(r, c, row, col, byPlayer)) {
+            return true;
+          }
+        }
+      }
+>>>>>>> 5082604 (Initial commit)
     }
     return false;
   }
 
   makeMove(fromR, fromC, toR, toC) {
     if (!this.moveCheck(fromR, fromC, toR, toC, this.currentPlayer)) return false;
+<<<<<<< HEAD
     
     const piece = this.board[fromR][fromC];
     const isPawn = piece.toUpperCase() === 'P';
     const isPromotionMove = isPawn && (toR === 0 || toR === 7);
     
+=======
+
+    const piece = this.board[fromR][fromC];
+    this.enPassantTarget = null;
+if (piece.toUpperCase() === 'P' && Math.abs(toR - fromR) === 2) {
+  this.enPassantTarget = {
+    row: (fromR + toR) / 2,
+    col: fromC
+  };
+}
+
+
+    const isPawn = piece.toUpperCase() === 'P';
+    const isKing = piece.toUpperCase() === 'K';
+    const isRook = piece.toUpperCase() === 'R';
+    const isPromotionMove = isPawn && (toR === 0 || toR === 7);
+
+    if (isKing && Math.abs(fromC - toC) === 2) {
+      return this.executeCastle(fromR, fromC, toR, toC);
+    }
+
+>>>>>>> 5082604 (Initial commit)
     if (isPromotionMove) {
       this.pendingPromotion = {
         fromR, fromC, toR, toC,
         movedPiece: piece,
         capturedPiece: this.board[toR][toC],
         previousState: JSON.parse(JSON.stringify(this.board)),
+<<<<<<< HEAD
         previousPlayer: this.currentPlayer
       };
       return true;
@@ -128,12 +281,74 @@ class ChessGame {
       return false;
     }
     
+=======
+        previousPlayer: this.currentPlayer,
+        previousCastling: JSON.parse(JSON.stringify(this.castlingRights))
+      };
+      return true;
+    }
+
+    if (isKing) {
+      this.castlingRights[this.currentPlayer].kingside = false;
+      this.castlingRights[this.currentPlayer].queenside = false;
+    }
+    if (isRook) {
+      if (fromC === 0) this.castlingRights[this.currentPlayer].queenside = false;
+      if (fromC === 7) this.castlingRights[this.currentPlayer].kingside = false;
+    }
+
+    return this.executeMove(fromR, fromC, toR, toC);
+  }
+
+  executeCastle(fromR, fromC, toR, toC) {
+    const isKingside = toC > fromC;
+    const rookFromC = isKingside ? 7 : 0;
+    const rookToC = isKingside ? 5 : 3;
+
+    const boardCopy = JSON.parse(JSON.stringify(this.board));
+    const playerCopy = this.currentPlayer;
+    const castlingCopy = JSON.parse(JSON.stringify(this.castlingRights));
+
+    this.board[toR][toC] = this.board[fromR][fromC];
+    this.board[fromR][fromC] = ' ';
+
+    this.board[toR][rookToC] = this.board[toR][rookFromC];
+    this.board[toR][rookFromC] = ' ';
+
+    this.castlingRights[this.currentPlayer].kingside = false;
+    this.castlingRights[this.currentPlayer].queenside = false;
+
+    this.currentPlayer = this.currentPlayer === 'w' ? 'b' : 'w';
+
+    if (this.isInCheck(this.currentPlayer === 'w' ? 'b' : 'w')) {
+      this.board = boardCopy;
+      this.currentPlayer = playerCopy;
+      this.castlingRights = castlingCopy;
+      return false;
+    }
+
+    const move = {
+      fromR, fromC, toR, toC,
+      movedPiece: boardCopy[fromR][fromC],
+      capturedPiece: ' ',
+      previousState: boardCopy,
+      previousPlayer: playerCopy,
+      previousCastling: castlingCopy,
+      isCastle: true,
+      rookFromR: fromR,
+      rookFromC,
+      rookToR: fromR,
+      rookToC
+    };
+
+>>>>>>> 5082604 (Initial commit)
     this.moveHistory.push(move);
     this.undoStack.push(move);
     this.redoStack = [];
     return true;
   }
 
+<<<<<<< HEAD
   promotePawn(pieceType) {
     if (!this.pendingPromotion) return false;
     
@@ -156,12 +371,74 @@ class ChessGame {
   undoMove() {
     if (this.undoStack.length === 0) return false;
     
+=======
+
+
+
+
+executeMove(fromR, fromC, toR, toC) {
+  const boardCopy = JSON.parse(JSON.stringify(this.board));
+  const playerCopy = this.currentPlayer;
+  const castlingCopy = JSON.parse(JSON.stringify(this.castlingRights));
+
+  let movedPiece = this.board[fromR][fromC];
+  let capturedPiece = this.board[toR][toC];
+  let isEnPassantCapture = false;
+
+  // 🟨 En Passant Check
+  if (
+    movedPiece.toUpperCase() === 'P' &&
+    this.enPassantTarget &&
+    toR === this.enPassantTarget.row &&
+    toC === this.enPassantTarget.col &&
+    this.board[toR][toC] === ' '
+  ) {
+    isEnPassantCapture = true;
+    capturedPiece = this.board[fromR][toC]; // the pawn being captured
+    this.board[fromR][toC] = ' '; // remove it
+  }
+
+  this.board[toR][toC] = movedPiece;
+  this.board[fromR][fromC] = ' ';
+
+  this.enPassantTarget = null;
+  this.currentPlayer = this.currentPlayer === 'w' ? 'b' : 'w';
+
+  if (this.isInCheck(playerCopy)) {
+    this.board = boardCopy;
+    this.currentPlayer = playerCopy;
+    this.castlingRights = castlingCopy;
+    return false;
+  }
+
+  const move = {
+    fromR, fromC, toR, toC,
+    movedPiece,
+    capturedPiece,
+    enPassant: isEnPassantCapture,
+    previousState: boardCopy,
+    previousPlayer: playerCopy,
+    previousCastling: castlingCopy
+  };
+
+  this.moveHistory.push(move);
+  this.undoStack.push(move);
+  this.redoStack = [];
+  return true;
+}
+
+
+  undoMove() {
+    if (this.undoStack.length === 0) return false;
+
+>>>>>>> 5082604 (Initial commit)
     const move = this.undoStack.pop();
     this.redoStack.push({
       fromR: move.fromR,
       fromC: move.fromC,
       toR: move.toR,
       toC: move.toC,
+<<<<<<< HEAD
       movedPiece: move.movedPiece,
       capturedPiece: move.capturedPiece,
       previousState: JSON.parse(JSON.stringify(this.board)),
@@ -172,18 +449,37 @@ class ChessGame {
     this.board = move.previousState;
     this.currentPlayer = move.previousPlayer;
     this.pendingPromotion = null;
+=======
+      movedPiece: this.board[move.toR][move.toC],
+      capturedPiece: move.capturedPiece,
+      previousState: JSON.parse(JSON.stringify(this.board)),
+      previousPlayer: this.currentPlayer,
+      previousCastling: JSON.parse(JSON.stringify(this.castlingRights))
+    });
+
+    // Restore the board state
+    this.board = JSON.parse(JSON.stringify(move.previousState));
+    this.currentPlayer = move.previousPlayer;
+    this.castlingRights = JSON.parse(JSON.stringify(move.previousCastling));
+
+>>>>>>> 5082604 (Initial commit)
     return true;
   }
 
   redoMove() {
     if (this.redoStack.length === 0) return false;
+<<<<<<< HEAD
     
+=======
+
+>>>>>>> 5082604 (Initial commit)
     const move = this.redoStack.pop();
     this.undoStack.push({
       fromR: move.fromR,
       fromC: move.fromC,
       toR: move.toR,
       toC: move.toC,
+<<<<<<< HEAD
       movedPiece: move.movedPiece,
       capturedPiece: move.capturedPiece,
       previousState: JSON.parse(JSON.stringify(this.board)),
@@ -194,12 +490,30 @@ class ChessGame {
     this.board[move.toR][move.toC] = move.promotion || move.movedPiece;
     this.board[move.fromR][move.fromC] = ' ';
     this.currentPlayer = this.currentPlayer === 'w' ? 'b' : 'w';
+=======
+      movedPiece: this.board[move.toR][move.toC],
+      capturedPiece: move.capturedPiece,
+      previousState: JSON.parse(JSON.stringify(this.board)),
+      previousPlayer: this.currentPlayer,
+      previousCastling: JSON.parse(JSON.stringify(this.castlingRights))
+    });
+
+    // Execute the move again
+    this.board[move.toR][move.toC] = move.movedPiece;
+    this.board[move.fromR][move.fromC] = ' ';
+    this.currentPlayer = this.currentPlayer === 'w' ? 'b' : 'w';
+
+>>>>>>> 5082604 (Initial commit)
     return true;
   }
 
   isInCheck(player) {
     let kingR = -1, kingC = -1;
+<<<<<<< HEAD
     const king = player === 'w' ? 'K' : 'k';
+=======
+    const king = player === 'w' ? 'K' : 'k'
+>>>>>>> 5082604 (Initial commit)
     for (let r = 0; r < this.SIZE; r++) {
       for (let c = 0; c < this.SIZE; c++) {
         if (this.board[r][c] === king) {
@@ -210,13 +524,21 @@ class ChessGame {
       }
       if (kingR !== -1) break;
     }
+<<<<<<< HEAD
     
+=======
+
+>>>>>>> 5082604 (Initial commit)
     const opp = player === 'w' ? 'b' : 'w';
     for (let r = 0; r < this.SIZE; r++) {
       for (let c = 0; c < this.SIZE; c++) {
         const piece = this.board[r][c];
         if ((opp === 'w' && piece === piece.toUpperCase()) ||
+<<<<<<< HEAD
             (opp === 'b' && piece === piece.toLowerCase())) {
+=======
+          (opp === 'b' && piece === piece.toLowerCase())) {
+>>>>>>> 5082604 (Initial commit)
           if (this.moveCheck(r, c, kingR, kingC, opp)) return true;
         }
       }
@@ -229,7 +551,11 @@ class ChessGame {
       for (let fromC = 0; fromC < this.SIZE; fromC++) {
         const piece = this.board[fromR][fromC];
         if ((player === 'w' && piece === piece.toUpperCase()) ||
+<<<<<<< HEAD
             (player === 'b' && piece === piece.toLowerCase())) {
+=======
+          (player === 'b' && piece === piece.toLowerCase())) {
+>>>>>>> 5082604 (Initial commit)
           for (let toR = 0; toR < this.SIZE; toR++) {
             for (let toC = 0; toC < this.SIZE; toC++) {
               if (this.moveCheck(fromR, fromC, toR, toC, player)) {
@@ -250,14 +576,91 @@ class ChessGame {
     return false;
   }
 
+<<<<<<< HEAD
   getGameStatus() {
     const inCheck = this.isInCheck(this.currentPlayer);
     const hasMoves = this.hasLegalMoves(this.currentPlayer);
     
+=======
+  getLegalMoves() {
+    const moves = [];
+    const player = this.currentPlayer;
+    for (let fromR = 0; fromR < this.SIZE; fromR++) {
+      for (let fromC = 0; fromC < this.SIZE; fromC++) {
+        const piece = this.board[fromR][fromC];
+        if ((player === 'w' && piece === piece.toUpperCase()) ||
+          (player === 'b' && piece === piece.toLowerCase())) {
+          for (let toR = 0; toR < this.SIZE; toR++) {
+            for (let toC = 0; toC < this.SIZE; toC++) {
+              if (this.moveCheck(fromR, fromC, toR, toC, player)) {
+                const backupFrom = this.board[fromR][fromC];
+                const backupTo = this.board[toR][toC];
+                this.board[toR][toC] = backupFrom;
+                this.board[fromR][fromC] = ' ';
+                const inCheck = this.isInCheck(player);
+                this.board[fromR][fromC] = backupFrom;
+                this.board[toR][toC] = backupTo;
+
+                if (!inCheck) {
+                  moves.push({ fromR, fromC, toR, toC });
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    return moves;
+  }
+
+  getGameStatus() {
+    const inCheck = this.isInCheck(this.currentPlayer);
+    const hasMoves = this.hasLegalMoves(this.currentPlayer);
+
+>>>>>>> 5082604 (Initial commit)
     if (!hasMoves) {
       return inCheck ? "Checkmate" : "Stalemate";
     }
     return inCheck ? "Check" : "Ongoing";
   }
+<<<<<<< HEAD
   
+=======
+
+  promotePawn(choice) {
+  const promotion = this.pendingPromotion;
+  if (!promotion) return false;
+
+  const isWhite = promotion.movedPiece === 'P';
+  const promoted = isWhite ? choice.toUpperCase() : choice.toLowerCase();
+
+  this.board = promotion.previousState;
+  this.currentPlayer = promotion.previousPlayer;
+  this.castlingRights = promotion.previousCastling;
+
+  this.board[promotion.toR][promotion.toC] = promoted;
+  this.board[promotion.fromR][promotion.fromC] = ' ';
+
+  const move = {
+    fromR: promotion.fromR,
+    fromC: promotion.fromC,
+    toR: promotion.toR,
+    toC: promotion.toC,
+    movedPiece: promotion.movedPiece,
+    capturedPiece: promotion.capturedPiece,
+    promotion: promoted,
+    previousState: promotion.previousState,
+    previousPlayer: promotion.previousPlayer,
+    previousCastling: promotion.previousCastling
+  };
+
+  this.moveHistory.push(move);
+  this.undoStack.push(move);
+  this.redoStack = [];
+  this.pendingPromotion = null;
+  this.currentPlayer = this.currentPlayer === 'w' ? 'b' : 'w';
+  return true;
+}
+
+>>>>>>> 5082604 (Initial commit)
 }
--- chess_ai.js
+++ chess_ai.js
@@ -1,5 +1,6 @@
 class ChessAI {
   constructor(color) {
+<<<<<<< HEAD
     this.color = color; 
     this.pieceValues = {
       'P': 100, 'N': 320, 'B': 330, 'R': 500, 'Q': 900, 'K': 20000,
@@ -7,12 +8,25 @@ class ChessAI {
     };
     this.moveHistory = [];
     this.positionCounts = new Map(); 
+=======
+    this.color = color; // 'w' or 'b'
+    this.pieceValues = {
+      'P': 100, 'N': 320, 'B': 330, 'R': 500, 'Q': 900, 'K': 200,
+      'p': -100, 'n': -320, 'b': -330, 'r': -500, 'q': -900, 'k': -200
+      };
+    this.moveHistory = []; // Track move history to avoid repetition
+    this.positionCounts = new Map(); // Track position occurrences
+>>>>>>> 5082604 (Initial commit)
   }
 
   evaluateBoard(game) {
     let score = 0;
     
+<<<<<<< HEAD
     
+=======
+    // Material evaluation
+>>>>>>> 5082604 (Initial commit)
     for (let row = 0; row < 8; row++) {
       for (let col = 0; col < 8; col++) {
         const piece = game.board[row][col];
@@ -22,12 +36,17 @@ class ChessAI {
       }
     }
     
+<<<<<<< HEAD
  
+=======
+    // Positional evaluation - add bonuses for good piece placement
+>>>>>>> 5082604 (Initial commit)
     for (let row = 0; row < 8; row++) {
       for (let col = 0; col < 8; col++) {
         const piece = game.board[row][col];
         if (piece === ' ') continue;
         
+<<<<<<< HEAD
         
         const centerDist = Math.max(Math.abs(3.5 - row), Math.abs(3.5 - col));
         const centerBonus = (4 - centerDist) * 5;
@@ -52,6 +71,32 @@ class ChessAI {
             score -= (row - 1) * 5;
             
             
+=======
+        // Center control bonus
+        const centerDist = Math.max(Math.abs(3.5 - row), Math.abs(3.5 - col));
+        const centerBonus = (4 - centerDist) * 5;
+        
+        if (piece === piece.toUpperCase()) { // White pieces
+          score += centerBonus;
+          
+          // Pawn structure bonus
+          if (piece === 'P') {
+            // Bonus for advanced pawns
+            score += (6 - row) * 5;
+            
+            // Bonus for connected pawns
+            if (col > 0 && game.board[row][col-1] === 'P') score += 10;
+            if (col < 7 && game.board[row][col+1] === 'P') score += 10;
+          }
+        } else { // Black pieces
+          score -= centerBonus;
+          
+          if (piece === 'p') {
+            // Bonus for advanced pawns
+            score -= (row - 1) * 5;
+            
+            // Bonus for connected pawns
+>>>>>>> 5082604 (Initial commit)
             if (col > 0 && game.board[row][col-1] === 'p') score -= 10;
             if (col < 7 && game.board[row][col+1] === 'p') score -= 10;
           }
@@ -80,9 +125,15 @@ class ChessAI {
 
         const currentMoves = game.getLegalMoves();
         
+<<<<<<< HEAD
        
         currentMoves.sort((a, b) => {
         
+=======
+        // Sort moves to improve alpha-beta pruning efficiency
+        currentMoves.sort((a, b) => {
+          // Try captures first
+>>>>>>> 5082604 (Initial commit)
           const aCapture = game.board[b.toR][b.toC] !== ' ' ? 1 : 0;
           const bCapture = game.board[a.toR][a.toC] !== ' ' ? 1 : 0;
           return bCapture - aCapture;
@@ -93,10 +144,17 @@ class ChessAI {
           for (const move of currentMoves) {
             const gameCopy = this.makeMoveCopy(game, move);
             
+<<<<<<< HEAD
             
             const newBoardState = gameCopy.getBoardState();
             if ((this.positionCounts.get(newBoardState) || 0) >= 2) {
               continue; 
+=======
+            // Avoid repeating positions
+            const newBoardState = gameCopy.getBoardState();
+            if ((this.positionCounts.get(newBoardState) || 0) >= 2) {
+              continue; // Skip moves that lead to repeated positions
+>>>>>>> 5082604 (Initial commit)
             }
             
             const evaluation = miniMax(gameCopy, depth - 1, false, alpha, beta, [...moveHistory, move]);
@@ -110,10 +168,17 @@ class ChessAI {
           for (const move of currentMoves) {
             const gameCopy = this.makeMoveCopy(game, move);
             
+<<<<<<< HEAD
             
             const newBoardState = gameCopy.getBoardState();
             if ((this.positionCounts.get(newBoardState) || 0) >= 2) {
               continue; 
+=======
+            // Avoid repeating positions
+            const newBoardState = gameCopy.getBoardState();
+            if ((this.positionCounts.get(newBoardState) || 0) >= 2) {
+              continue; // Skip moves that lead to repeated positions
+>>>>>>> 5082604 (Initial commit)
             }
             
             const evaluation = miniMax(gameCopy, depth - 1, true, alpha, beta, [...moveHistory, move]);
@@ -125,7 +190,11 @@ class ChessAI {
         }
       };
 
+<<<<<<< HEAD
       
+=======
+      // Find best move with additional criteria
+>>>>>>> 5082604 (Initial commit)
       let bestMove = null;
       let bestValue = this.color === 'w' ? -Infinity : Infinity;
       let candidateMoves = [];
@@ -133,7 +202,11 @@ class ChessAI {
       for (const move of legalMoves) {
         const gameCopy = this.makeMoveCopy(game, move);
         
+<<<<<<< HEAD
        
+=======
+        // Skip moves that lead to repeated positions
+>>>>>>> 5082604 (Initial commit)
         const newBoardState = gameCopy.getBoardState();
         if ((this.positionCounts.get(newBoardState) || 0) >= 2) {
           continue;
@@ -141,33 +214,57 @@ class ChessAI {
         
         const moveValue = miniMax(gameCopy, depth - 1, this.color === 'b', -Infinity, Infinity);
         
+<<<<<<< HEAD
        
+=======
+        // Collect all candidate moves with their values
+>>>>>>> 5082604 (Initial commit)
         candidateMoves.push({ move, value: moveValue });
       }
 
       if (candidateMoves.length === 0) {
+<<<<<<< HEAD
         
         return legalMoves[Math.floor(Math.random() * legalMoves.length)];
       }
 
     
+=======
+        // If all moves lead to repetition, pick one randomly
+        return legalMoves[Math.floor(Math.random() * legalMoves.length)];
+      }
+
+      // Sort candidate moves by value
+>>>>>>> 5082604 (Initial commit)
       candidateMoves.sort((a, b) => 
         this.color === 'w' ? b.value - a.value : a.value - b.value
       );
 
+<<<<<<< HEAD
       
+=======
+      // Pick from top 3 moves to add variety
+>>>>>>> 5082604 (Initial commit)
       const topMoves = candidateMoves.slice(0, 3);
       bestMove = topMoves[Math.floor(Math.random() * topMoves.length)].move;
 
       if (bestMove) {
+<<<<<<< HEAD
        
+=======
+        // Handle pawn promotion - don't always promote to queen
+>>>>>>> 5082604 (Initial commit)
         const piece = game.board[bestMove.fromR][bestMove.fromC];
         const isPawn = piece.toUpperCase() === 'P';
         const isPromotion = isPawn && (bestMove.toR === 0 || bestMove.toR === 7);
         
         let promotedTo = null;
         if (isPromotion) {
+<<<<<<< HEAD
        
+=======
+          // 80% chance to promote to queen, 20% to other pieces
+>>>>>>> 5082604 (Initial commit)
           const rand = Math.random();
           if (this.color === 'w') {
             promotedTo = rand < 0.8 ? 'Q' : ['R', 'B', 'N'][Math.floor((rand - 0.8) * 3 / 0.2)];
@@ -199,7 +296,11 @@ class ChessAI {
     gameCopy.undoStack = [...game.undoStack];
     gameCopy.redoStack = [...game.redoStack];
     
+<<<<<<< HEAD
    
+=======
+    // Handle promotion in the copy if needed
+>>>>>>> 5082604 (Initial commit)
     const piece = gameCopy.board[move.fromR][move.fromC];
     const isPawn = piece.toUpperCase() === 'P';
     const isPromotion = isPawn && (move.toR === 0 || move.toR === 7);
@@ -212,4 +313,8 @@ class ChessAI {
     gameCopy.executeMove(move.fromR, move.fromC, move.toR, move.toC, promotionPiece);
     return gameCopy;
   }
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> 5082604 (Initial commit)
